20ee769a670ede44f430f7700c7a1c8f6770a868
yejiyun <20172062@sungshin.ac.kr>
2021-11-22 20:06:54 +0900

    #28: modify scenedelegate entryo point

--- everyBody/everyBody-iOS/Resource/Support/SceneDelegate.swift
+++ everyBody/everyBody-iOS/Resource/Support/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .white
-        let tabbarViewController = PreferenceViewController()
+        let tabbarViewController = TabbarViewController()
         window?.rootViewController = UINavigationController(rootViewController: tabbarViewController)
         window?.makeKeyAndVisible()
     }
