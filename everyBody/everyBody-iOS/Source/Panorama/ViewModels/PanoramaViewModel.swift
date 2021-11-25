3b8a3133aab78f87e976825215ae5020c94dcbde
kong <1018dbrud@gmail.com>
2021-11-26 04:44:02 +0900

    #20: 뷰모델에 삭제 배열 추가

--- everyBody/everyBody-iOS/Source/Panorama/ViewModels/PanoramaViewModel.swift
+++ everyBody/everyBody-iOS/Source/Panorama/ViewModels/PanoramaViewModel.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class PanoramaViewModel {
     var albumTitle: String = "예꽁이의 섹시눈바디"
-    var phothArray: [UIImage] = [Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image]
+    var phothArray: [UIImage] = [Asset.Image.del.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.add.image, Asset.Image.sample.image, Asset.Image.back.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image,
+                                 Asset.Image.del.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.add.image, Asset.Image.sample.image, Asset.Image.back.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image, Asset.Image.sample.image]
 
     var wholebody: [UIImage] = []
     var upperbody: [UIImage] = []
     var lowerbody: [UIImage] = []
+    
+    var deleteArray: [Int] = []
 }
