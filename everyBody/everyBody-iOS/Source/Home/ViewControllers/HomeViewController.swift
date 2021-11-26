9dc9f9e2d9dbe74168f6f6c629413d6df29849dc
kong <1018dbrud@gmail.com>
2021-11-27 06:57:33 +0900

    #39: add album view
    메인 앨범 뷰를 추가했습니다!

--- everyBody/everyBody-iOS/Source/Home/ViewControllers/HomeViewController.swift
+++ everyBody/everyBody-iOS/Source/Home/ViewControllers/HomeViewController.swift
@@ -7,21 +7,74 @@
 
 import UIKit
 
-class HomeViewController: BaseViewController {
+import SnapKit
+import Then
 
-    private lazy var albumButton = UIButton().then {
+class HomeViewController: BaseViewController {
+    
+    // MARK: - UI Components
+
+    private lazy var cameraButton = UIButton().then {
         $0.backgroundColor = Asset.Color.keyPurple.color
         $0.setImage(Asset.Image.photoCamera.image, for: .normal)
         $0.makeRounded(radius: 28)
         $0.addTarget(self, action: #selector(pushToCameraViewController), for: .touchUpInside)
     }
     
+    private lazy var emptyView = UIView()
+    
+    private lazy var nicknameLabel = UILabel().then {
+        $0.text = "예꽁이"
+        $0.font = .nbFont(type: .subtitle)
+    }
+    
+    private lazy var mottoLabel = UILabel().then {
+        $0.text = "난 퀸이될거야."
+        $0.font = .nbFont(type: .body3)
+        $0.textColor = Asset.Color.gray60.color
+    }
+    
+    private lazy var emptyDescription = UILabel().then {
+        $0.text = "앨범이 없습니다.\n지금 앨범을 만들어 기록해보세요."
+        $0.textAlignment = .center
+        $0.numberOfLines = 0
+        $0.font = .nbFont(type: .body2)
+        $0.textColor = Asset.Color.gray50.color
+    }
+    
+    private var createButton = UIButton().then {
+        $0.setTitle("앨범 생성", for: .normal)
+        $0.backgroundColor = Asset.Color.gray50.color
+        $0.makeRounded(radius: 28)
+    }
+    
+    private lazy var albumCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 32
+        layout.minimumInteritemSpacing = 11
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        layout.itemSize = CGSize(width: (Constant.Size.screenWidth - 51) / 2, height: 211)
+        $0.register(AlbumCollectionViewCell.self)
+        $0.backgroundColor = .white
+        $0.collectionViewLayout = layout
+    }
+    
+    // MARK: - Properties
+    
+    var viewModel = AlbumViewModel()
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initNavigationBar()
-        setupContraint()
+        setupCollectionView()
+        setupViewHierarchy()
+        setupConstraint()
     }
+    
+    // MARK: - Methods
     
     private func initNavigationBar() {
         navigationController?.initNavigationBar(
@@ -34,6 +87,17 @@ class HomeViewController: BaseViewController {
                            #selector(switchAlbumMode)]
         )
     }
+    
+    private func setupCollectionView() {
+        albumCollectionView.dataSource = self
+    }
+    
+    private func setupViewHierarchy() {
+        view.addSubviews(cameraButton, nicknameLabel, mottoLabel, emptyView, albumCollectionView)
+        emptyView.addSubviews(emptyDescription, createButton)
+    }
+
+    // MARK: - Actions
     
     @objc
     private func switchAlbumMode() {
@@ -61,16 +125,62 @@ class HomeViewController: BaseViewController {
     
 }
 
+// MARK: - Layout
+
+// MARK: - Extension
+
 extension HomeViewController {
-    
-    private func setupContraint() {
-        view.addSubview(albumButton)
-        
-        albumButton.snp.makeConstraints {
+    private func setupConstraint() {
+        cameraButton.snp.makeConstraints {
             $0.trailing.equalToSuperview().inset(20)
             $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(20)
             $0.width.height.equalTo(56)
         }
+        
+        nicknameLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(8)
+            $0.leading.equalTo(20)
+            
+        }
+        
+        mottoLabel.snp.makeConstraints {
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(nicknameLabel)
+        }
+        
+        albumCollectionView.snp.makeConstraints {
+            $0.top.equalTo(mottoLabel.snp.bottom).offset(20)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        emptyView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalTo(270 * Constant.Size.screenWidth / Constant.Size.figmaWidth)
+            $0.height.equalTo(136 * Constant.Size.screenWidth / Constant.Size.figmaWidth)
+        }
+        
+        emptyDescription.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
+        
+        createButton.snp.makeConstraints {
+            $0.height.equalTo(56)
+            $0.width.equalTo(121)
+            $0.centerX.bottom.equalToSuperview()
+        }
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.dummy.count
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: AlbumCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+        cell.style = .album
+        cell.setData(album: viewModel.dummy[indexPath.row])
+        return cell
     }
     
 }
