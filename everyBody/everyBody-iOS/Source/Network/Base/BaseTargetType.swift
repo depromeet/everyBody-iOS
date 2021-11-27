f3f7857688c59d829d0c902cdf7bb3c677b591e5
yejiyun <20172062@sungshin.ac.kr>
2021-11-27 10:57:37 +0900

    #37: Moya base setting
    - MoyaProvider MultiTarget 사용

---
+++ everyBody/everyBody-iOS/Source/Network/Base/BaseTargetType.swift
@@ -0,0 +1,36 @@
+//
+//  BaseTargetType.swift
+//  everyBody-iOS
+//
+//  Created by 윤예지 on 2021/11/26.
+//
+
+import Moya
+import Foundation
+
+protocol BaseTargetType: Moya.TargetType {
+    associatedtype ResultModel: Decodable
+}
+
+extension BaseTargetType {
+
+    var baseURL: URL {
+        // swiftlint:disable force_cast
+        let url = Bundle.main.infoDictionary?["API_URL"] as! String
+        return URL(string: "https://" + url)!
+    }
+
+    var headers: [String: String]? {
+        // swiftlint:disable force_cast
+        let dummyToken = Bundle.main.infoDictionary?["TOKEN"] as! String
+        let header = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + dummyToken]
+        return header
+    }
+
+    var sampleData: Data {
+        return Data()
+    }
+
+}
