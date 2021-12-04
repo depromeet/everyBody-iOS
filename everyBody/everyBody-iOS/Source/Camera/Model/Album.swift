0bf1348fec3b0a73283178dbd82753dda9e05b3e
kong <1018dbrud@gmail.com>
2021-12-04 22:53:21 +0900

    #50: add AlbumModel Initializer
    앨범 모델에 이니셜라이저를 추가했습니다

--- everyBody/everyBody-iOS/Source/Camera/Model/Album.swift
+++ everyBody/everyBody-iOS/Source/Camera/Model/Album.swift
@@ -24,6 +24,15 @@ struct Album: Codable {
         case albumDescription = "description"
         case pictures
     }
+    
+    init(id: Int = 0, name: String = "", thumbnailURL: String? = nil, createdAt: String = "", albumDescription: String = "", pictures: Pictures = Pictures()) {
+        self.id = id
+        self.name = name
+        self.thumbnailURL = thumbnailURL
+        self.createdAt = createdAt
+        self.albumDescription = albumDescription
+        self.pictures = pictures
+    }
 }
 
 // MARK: - Pictures
@@ -32,6 +41,12 @@ struct Pictures: Codable {
     let lower: [PictureInfo]
     let upper: [PictureInfo]
     let whole: [PictureInfo]
+    
+    init(lower: [PictureInfo] = [], upper: [PictureInfo] = [], whole: [PictureInfo] = []) {
+        self.lower = lower
+        self.upper = upper
+        self.whole = whole
+    }
 }
 
 // MARK: - PictureInfo
