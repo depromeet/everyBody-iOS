b168455748a19bc7cc2352be2d80ee533ac3c70a
yejiyun <20172062@sungshin.ac.kr>
2022-02-09 21:54:18 +0900

    #143: 빌드 환경 분리 및 런치 로직 변경

--- Noonbody/Noonbody/Source/Presentation/Launch/ViewController/LaunchScreenViewController.swift
+++ Noonbody/Noonbody/Source/Presentation/Launch/ViewController/LaunchScreenViewController.swift
@@ -37,22 +37,18 @@ class LaunchScreenViewController: UIViewController {
     private func addObserver() {
         if UserManager.userId == nil {
             NotificationCenter.default.addObserver(self,
-                                                   selector: #selector(checkFirstLaunch),
+                                                   selector: #selector(signUp),
                                                    name: Notification.Name("setFcmToken"),
                                                    object: nil)
         } else {
-            requestSignIn()
+            pushToHomeViewController()
         }
     }
     
     @objc
-    private func checkFirstLaunch(notification: NSNotification) {
+    private func signUp(notification: NSNotification) {
         if let fcmToken = notification.object as? String {
-            if UserManager.userId == nil {
-                requestSignUp(fcmToken: fcmToken)
-            } else {
-                requestSignIn()
-            }
+            requestSignUp(fcmToken: fcmToken)
         }
     }
     
@@ -62,7 +58,7 @@ class LaunchScreenViewController: UIViewController {
     }
     
     private func requestSignUp(fcmToken: String) {
-        AuthService.shared.postSignUp(request: SignUpRequestModel(password: uuid,
+        AuthService.shared.postSignUp(request: SignUpRequestModel(password: fcmToken,
                                                                   device: Device(deviceToken: fcmToken,
                                                                                  pushToken: fcmToken))) { response in
             switch response {
@@ -72,7 +68,7 @@ class LaunchScreenViewController: UIViewController {
                     UserManager.nickname = data.nickname
                     UserManager.motto = data.motto
                     UserManager.profile = data.profileImage
-                    self.requestSignIn()
+                    self.requestSignIn(fcmToken: fcmToken)
                 }
             case .failure(let err):
                 print(err)
@@ -81,10 +77,10 @@ class LaunchScreenViewController: UIViewController {
         
     }
     
-    private func requestSignIn() {
+    private func requestSignIn(fcmToken: String) {
         guard let userId = UserManager.userId else { return }
         AuthService.shared.postSignIn(request: SignInRequestModel(userId: userId,
-                                                                  password: uuid)) { response in
+                                                                  password: fcmToken)) { response in
             switch response {
             case .success(let data):
                 if let data = data {
