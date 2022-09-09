1af8a26485c43ed9226f72f6696922d6031f81f4
kong <1018dbrud@gmail.com>
2022-09-09 19:37:34 +0900

    #179: background에서 foreground 진입시 생체인증 추가, 잠금해제 팝업 로직 수정

--- Noonbody/Noonbody/Resource/Support/SceneDelegate.swift
+++ Noonbody/Noonbody/Resource/Support/SceneDelegate.swift
@@ -12,6 +12,7 @@ import Siren
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private var appSwitcherModeImageView = UIImageView()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -47,16 +48,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        appSwitcherModeImageView.removeFromSuperview()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        if UserManager.biometricAuthentication {
+            setupAppSwitcherMode()
+        }
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        if UserManager.biometricAuthentication {
+            switch scene.activationState {
+            case .background: // 한번 백그라운드에 갔다온 경우, 다시 생체인증 요구
+                evaluateAuthentication()
+            default:
+                break
+            }
+        }
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
@@ -68,4 +81,47 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
+}
+
+extension SceneDelegate {
+    
+    private func evaluateAuthentication() {
+        LocalAuthenticationService.shared.evaluateAuthentication { response, error in
+            DispatchQueue.main.async {
+                if response {
+                    self.window?.rootViewController?.presentedViewController?.dismiss(animated: false)
+                } else if error != nil {
+                    let popUp = AuthenticationPopupViewController()
+                    popUp.delegate = self
+                    popUp.modalTransitionStyle = .crossDissolve
+                    popUp.modalPresentationStyle = .overFullScreen
+                    self.window?.rootViewController?.present(popUp, animated: false)
+                }
+            }
+        }
+    }
+    
+    private func setupAppSwitcherMode() {
+        guard let window = window else { return }
+        appSwitcherModeImageView = UIImageView(frame: window.frame)
+        appSwitcherModeImageView.image = Asset.Image.logo.image
+        appSwitcherModeImageView.contentMode = .center
+        appSwitcherModeImageView.backgroundColor = Asset.Color.keyPurple.color
+        window.addSubview(appSwitcherModeImageView)
+    }
+    
+}
+
+extension SceneDelegate: PopUpActionProtocol {
+    
+    func cancelButtonDidTap(_ button: UIButton) {
+        LocalAuthenticationService.shared.evaluateAuthentication { response, _ in
+            DispatchQueue.main.async {
+                if response {
+                    self.window?.rootViewController?.presentedViewController?.dismiss(animated: false)
+                }
+            }
+        }
+    }
+    
 }
